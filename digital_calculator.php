453bae49756e9fc0ac3f3441c6caa3bfc934364b
Priyanka Kulthe <kulthe.priyanka@gmail.com>
2019-01-04 01:21:52 +0530

    Commit Task 3

--- digital_calculator.php
+++ digital_calculator.php
@@ -5,7 +5,7 @@ class Digital_calculator
     {
         $arguments              = array();
         $arr_allowed_operations = array('sum', 'add');
-        $pattern                = '/[,]/';
+        $pattern                = '/[,;]/';
     }
 
     public function index($arguments)
